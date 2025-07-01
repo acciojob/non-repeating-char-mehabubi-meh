965bad231872b7be2062b5ca09d76d9aee3d9111
mehabubi-meh <mehabubimeh0708@gmail.com>
2025-07-01 13:26:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
+  let map = {};
+
+  // Count each character's frequency
+  for (let char of str) {
+    map[char] = (map[char] || 0) + 1;
+  }
+
+  // Find the first character with frequency 1
+  for (let char of str) {
+    if (map[char] === 1) {
+      return char;
+    }
+  }
+
+  return null;
 }
+
 const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+alert(firstNonRepeatedChar(input));
